0038664db00c483fab914aba90532d6f47ff7e2c
pacomesimon <smbonimpa2011@gmail.com>
2022-03-28 18:00:07 +0200

    D1_majority.js initiation

---
+++ D1_majority.js
@@ -0,0 +1,23 @@
+function majority(inputArr){
+    var inputSet = new Set(inputArr);
+    var inputArrValues = [... inputSet];
+    for (var i=0; i<inputArrValues.length; i++){
+        var counter = 0;
+        for (var j=0; j<inputArr.length; j++){
+            if(inputArr[j]==inputArrValues[i]){
+                counter++;
+            }
+        }
+        if(counter > inputArr.length/2){
+            console.log("   -the majority element: ",inputArrValues[i],
+                        "\n   -occurences: ",counter,
+                        "\n   -total array size: ", inputArr.length);
+            return true;
+        }
+    }
+    console.log("the input array has NO MAJORITY ELEMENT!");
+    return false;
+}
+
+var inputArr=[3,1,3,4,4,5,3,5,3,3,3,6,3];
+majority(inputArr)
